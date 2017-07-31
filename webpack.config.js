811adbbe4da67b9ba8662943a1359eca4d17a60b
Shabbir Hussain Bohra <sjametwala@gmail.com>
2017-07-31 18:04:35 +0530

    blogsite: new webpeck configurations. * added web-dev-server, removed server and express. * Used HtmlWebpackPlugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 var webpack = require('webpack');
 var path = require('path');
 
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
@@ -8,7 +16,8 @@ var config = {
 	entry: APP_DIR + '/index.js',
 	output: {
 		path: BUILD_DIR,
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		publicPath: '/public'
 	},
 
 	devtool: "source-map",
@@ -26,6 +35,17 @@ var config = {
 				loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
 			}
 		]
+	},
+
+	plugins: [
+		HtmlWebpackPluginConfig
+	],
+
+	devServer: {
+		port: 3000,
+		historyApiFallback: {
+			index: '/public/index.html'
+		}
 	}
 };
 
